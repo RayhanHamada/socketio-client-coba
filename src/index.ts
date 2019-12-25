a329f9de4256062b889ed26bb4ea3b5502de71c8
RayhanHamada <hamadadesk1999@gmail.com>
2019-12-26 01:54:10 +0700

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log("hello world!");
+import io from "socket.io-client";
+
+const socket = io("http://localhost:3000", {
+  autoConnect: true
+});
+
+document.getElementById("doEmit")?.addEventListener("click", ev => {
+  if (socket.connected) {
+    socket.emit("clicked", {
+      nama: "Rayhan Hamada"
+    });
+  } else {
+    console.log(`We're not connected`);
+  }
+});
+
+document.getElementById("doCloseConnection")?.addEventListener("click", ev => {
+  if (socket.connected) {
+    socket.disconnect();
+  } else {
+    console.log(`socket already disconnected`);
+  }
+});
+
+document.getElementById("doOpenConnection")?.addEventListener("click", ev => {
+  if (socket.disconnected) {
+    socket.connect();
+  } else {
+    console.log(`socket already connected`);
+  }
+});
